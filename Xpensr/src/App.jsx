ed96db30651d7d89d3d44cd6f73ecd599b118e00
ssrawat1 <ssr911999@gmail.com>
2024-06-11 00:51:42 +0530

    Implement routing with Outlet

---
+++ Xpensr/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
+
+export default App;
--- Xpensr/src/main.jsx
+++ Xpensr/src/main.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import MyApp from "./Components/MyApp";
 import "./index.css";
+import Home from "./Components/Home";
+import ExpenseReport from "./Components/ExpenseReport/ExpenseReport";
+import LogExpenses from "./Components/LogExpenses/LogExpenses";
+import App from "./App";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Home />} />
+      <Route path="xpensr" element={<LogExpenses />} />
+      <Route path="summary" element={<ExpenseReport />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MyApp />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
